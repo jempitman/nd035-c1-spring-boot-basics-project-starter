ce675ea982d8472f9fc39a2d9daf3b345b62545f
jempitman <80392341+jempitman@users.noreply.github.com>
2021-07-07 14:33:19 +0200

    Removed duplicate AuthenticationService method

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -8,40 +10,39 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
-public class AuthenticationService {
 
-    @Service
-    public class AuthenticationService implements AuthenticationProvider {
+@Service
+public class AuthenticationService implements AuthenticationProvider {
 
-        private UserMapper userMapper;
-        private HashService hashService;
+    private UserMapper userMapper;
+    private HashService hashService;
 
-        public AuthenticationService(UserMapper userMapper, HashService hashService) {
-            this.userMapper = userMapper;
-            this.hashService = hashService;
-        }
+    public AuthenticationService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
 
-        @Override
-        public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-            String username = authentication.getName();
-            String password = authentication.getCredentials().toString();
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
 
-            User user = userMapper.getUser(username);
-            if (user != null){
-                String encodedSalt = user.getSalt();
-                String hashedPassword = hashService.getHashedValue(password, encodedSalt);
-                if (user.getPassword().equals(hashedPassword)){
-                    return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
-                }
+        User user = userMapper.getUser(username);
+        if (user != null){
+            String encodedSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+            if (user.getPassword().equals(hashedPassword)){
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
             }
-
-            return null;
-
         }
 
-        @Override
-        public boolean supports(Class<?> authentication){
-            return authentication.equals(UsernamePasswordAuthenticationToken.class);
-        }
+        return null;
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication){
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
 
 }
